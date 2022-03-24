f1b9611c1aa18e91af770b8dd3c6c390334c4423
Artem <arstrel@gmail.com>
2022-03-23 21:34:16 -0700

    Working on making server-side requests from one service to ingress-nginx

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default function Main() {
-  return <h1>Landing page</h1>;
+import axios from 'axios';
+
+function LandingPage({ currentUser }) {
+  return <h1>Landing page: {currentUser.email}</h1>;
 }
+
+export async function getServerSideProps({ req }) {
+  try {
+    // http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const currentUser = await axios
+      .get(
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+        {
+          headers: {
+            Host: req.headers.host, // 'ticketing.dev',
+            cookie: req.headers.cookie,
+          },
+        }
+      )
+      .then((res) => res.data);
+
+    return { props: currentUser };
+  } catch (err) {
+    console.log(err);
+    return { props: { email: 'Not good' } };
+  }
+}
+
+export default LandingPage;
