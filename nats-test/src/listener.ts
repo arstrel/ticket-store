b2f0e6386e07046fd62ccabcd75c91bc7ce70e79
Artem <arstrel@gmail.com>
2022-04-02 18:12:44 -0700

    Added manual acknoledgement and query groups to test NATS app

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,19 +1,37 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+const id = randomBytes(6).toString('hex');
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', id, {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+      msg.ack();
     }
   });
 });
+
+// Graceful termination
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
