6d0fe00e9d1d967bb1d88303c77b3822363251a2
Artem <arstrel@gmail.com>
2022-04-02 21:03:28 -0700

    Test NATS project: durable subscribtion/deliver all available options

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,7 +14,11 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
 
   const subscription = stan.subscribe(
     'ticket:created',
