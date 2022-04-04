e70b338400b05c6b226cb933da2e097a13820a85
Artem <arstrel@gmail.com>
2022-04-04 12:16:37 -0700

    Refactor NATS test project to custom classes and promises

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,31 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const message = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  // const message = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
 
-  stan.publish('ticket:created', message, () => {
-    console.log('Event published');
-  });
+  // stan.publish('ticket:created', message, () => {
+  //   console.log('Event published');
+  // });
+
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 10,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
