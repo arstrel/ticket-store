f659101f5ca5346f1764cdcdbdcf5cf7517b9772
Artem <arstrel@gmail.com>
2022-04-21 16:16:40 -0700

    investigating nextjs issue

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 export function buildServersideClient({ req }) {
+  console.log({
+    // eslint-disable-next-line no-undef
+    baseURL: process?.env?.BASE_URL || 'http://www.useticketing.store/',
+  });
   // http://SERVICENAME.NAMESPACE.svc.cluster.local
   return axios.create({
     // eslint-disable-next-line no-undef
