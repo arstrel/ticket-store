e3dfd2efff18bdeb07f6121f7ce5bff62db72007
Artem <arstrel@gmail.com>
2022-04-22 15:45:33 -0700

    client changes test 1

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,11 +1,24 @@
-// import AuthForm from 'components/AuthForm';
+import AuthForm from 'components/AuthForm';
 // import { AUTH_FORM_TYPE } from 'appConstants';
+
+const AUTH_FORM_TYPE = {
+  signup: {
+    url: '/api/users/signup',
+    title: 'Sign Up',
+    actionLabel: 'Sign Up',
+  },
+  signin: {
+    url: '/api/users/signin',
+    title: 'Sign In',
+    actionLabel: 'Sign In',
+  },
+};
 
 export default function Signup() {
   return (
     <div className="container">
       <h1>Sign up</h1>
-      {/* <AuthForm type={AUTH_FORM_TYPE.signup} /> */}
+      <AuthForm type={AUTH_FORM_TYPE.signup} />
     </div>
   );
 }
