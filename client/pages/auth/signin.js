5d4e78fdc453b4f229e7dbc578bdc97978ceaa59
Artem <arstrel@gmail.com>
2022-04-22 15:39:57 -0700

    client test

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,10 +1,11 @@
-import AuthForm from 'components/AuthForm.js';
-import { AUTH_FORM_TYPE } from 'appConstants';
+// import AuthForm from 'components/AuthForm';
+// import { AUTH_FORM_TYPE } from 'appConstants';
 
 export default function Signin() {
   return (
     <div className="container">
-      <AuthForm type={AUTH_FORM_TYPE.signin} />
+      <h1>Signin</h1>
+      {/* <AuthForm type={AUTH_FORM_TYPE.signin} /> */}
     </div>
   );
 }
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,10 +1,11 @@
-import AuthForm from 'components/AuthForm';
-import { AUTH_FORM_TYPE } from 'appConstants';
+// import AuthForm from 'components/AuthForm';
+// import { AUTH_FORM_TYPE } from 'appConstants';
 
 export default function Signup() {
   return (
     <div className="container">
-      <AuthForm type={AUTH_FORM_TYPE.signup} />
+      <h1>Sign up</h1>
+      {/* <AuthForm type={AUTH_FORM_TYPE.signup} /> */}
     </div>
   );
 }
